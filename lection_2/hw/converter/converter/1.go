5b0f89ed3b73ee1ab333cf404139cd412ce8d8fd
Dzmitry <mr.priskoka@yandex.com>
2022-11-21 21:02:53 +0300

    Implement IntToRoman converter

--- lection_2/hw/converter/converter/1.go
+++ lection_2/hw/converter/converter/1.go
@@ -1,2 +1,39 @@
 package converter
 
+import (
+	"strings"
+)
+
+type converterImpl struct {
+	symbols []string
+	values  []int
+}
+
+type Converter interface {
+	IntToRoman(input int) (string, error)
+}
+
+func (impl *converterImpl) IntToRoman(num int) (string, error) {
+	var res strings.Builder
+
+	for i, val := range impl.values {
+		if num >= val {
+			var count int = num / val
+			res.WriteString(strings.Repeat(impl.symbols[i], count))
+			num -= count * val
+		}
+	}
+
+	return res.String(), nil
+}
+
+func NewConverter(values []int, symbols []string) Converter {
+	return &converterImpl{
+		values:  values,
+		symbols: symbols,
+	}
+}
+
+func IntToRoman(input int) {
+
+}
--- lection_2/hw/converter/main_test.go
+++ lection_2/hw/converter/main_test.go
@@ -19,14 +19,14 @@ func TestIntegerToRoman(t *testing.T) {
 		{integer: 123, roman: "CXXIII"},
 		{integer: 120, roman: "CXX"},
 	}
-	// TODO Реализовать Калькулятор
+
 	values := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 	symbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 	converterInterface := converter.NewConverter(values, symbols)
 
 	for _, test := range cases {
 
-		actualRoman := converterInterface.IntToRoman(test.integer)
+		actualRoman, _ := converterInterface.IntToRoman(test.integer)
 
 		if actualRoman != test.roman {
 			t.Error("Integer:", test.integer,
